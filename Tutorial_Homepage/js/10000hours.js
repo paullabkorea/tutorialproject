b6ff3ac4ec17eebc9b6a76fd113c5d46dceaf079
u_zeenee <dbwls982@gmail.com>
2021-02-24 12:34:11 +0900

    1만시간의법칙 추가

---
+++ Tutorial_Homepage/js/10000hours.js
@@ -0,0 +1,73 @@
+
+const startButton = document.querySelector(".start_btn");
+const result = document.querySelector(".result");
+const modal = document.querySelector("#modal");
+const openButton = document.querySelector(".modal_btn");
+const closeButton = document.querySelector(".close_btn");
+const shareButton = document.querySelector(".share_btn");
+const loading = document.querySelector(".result_loading");
+
+function calculator() {
+    const fieldValue = document.querySelector("#field_value");
+    let timeValue = document.querySelector("#time_value");
+    let timeValue_int = parseInt(timeValue.value, 10);
+
+    const fieldResult = document.querySelector(".field_result");
+    const timeResult = document.querySelector(".time_result");
+
+    if(fieldValue.value == "") {
+        alert('입력되지 않았습니다.');
+        fieldValue.focus();
+        return false;
+    } else if (timeValue.value == "") {
+        alert('입력되지 않았습니다.');
+        timeValue.focus();
+        return false;
+    } else if (timeValue_int > 24) {
+        alert('잘못된 값입니다. 24이하의 값을 입력해 주세요.');
+        return false;
+    }
+
+    result.style.display = "none";
+    loading.style.display = "flex";
+
+    setTimeout(function() {
+        loading.style.display = "none";
+        result.style.display = "flex";
+        fieldResult.innerText = fieldValue.value;
+        timeResult.innerText = parseInt((10000/timeValue_int), 10);
+    }, 1800);   
+}
+
+function openModal() {
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if(event.target == modal) {
+        closeModal();
+    }
+};
+
+function copyUrl() {
+    let url = window.location.href;
+    let tmp = document.createElement('input');
+    
+
+    document.body.appendChild(tmp);
+    tmp.value = url;
+    tmp.select();
+	document.execCommand("copy");
+    document.body.removeChild(tmp);
+    
+	alert("URL이 복사되었습니다"); 
+}
+
+shareButton.addEventListener('click', copyUrl);
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+startButton.addEventListener("click", calculator);
