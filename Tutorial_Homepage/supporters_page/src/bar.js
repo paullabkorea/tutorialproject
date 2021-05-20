fbaa2462d504656bce132170eac8fcb37cf8adbd
tmdals8030 <48111893+tmdals8030@users.noreply.github.com>
2021-05-20 11:37:20 +0900

    날짜수정, js 수정

--- Tutorial_Homepage/supporters_page/src/bar.js
+++ Tutorial_Homepage/supporters_page/src/bar.js
@@ -2,6 +2,7 @@ const body = document.querySelector("body");
 const barBtn = document.querySelector(".menu-bar");
 const nav = document.querySelector("nav");
 const backBtn = nav.querySelector("button");
+const applyBtn = document.querySelector(".apply-btn");
 
 function showNav() {
   const navName = nav.className;
@@ -10,6 +11,7 @@ function showNav() {
   } else {
     nav.classList.add("showing-nav");
     body.style.overflow = "hidden";
+    applyBtn.style.visibility = "hidden";
   }
 }
 
@@ -17,4 +19,5 @@ barBtn.addEventListener("click", showNav);
 backBtn.addEventListener("click", () => {
   nav.classList.remove("showing-nav");
   body.style.overflow = "auto";
+  applyBtn.style.visibility = "visible";
 });
